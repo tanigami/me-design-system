01cac4653db5dbfceeb56750da762b0852c3d71e
tanigami <tanigami@gmail.com>
2023-04-18 19:15:10 +0900

    a

--- article-body/ArticleBodyParagraph.tsx
+++ article-body/ArticleBodyParagraph.tsx
@@ -6,7 +6,6 @@ import data from '../sd-input/src/sd-input.json';
 const typography = data.article.body.paragraph.typography;
 console.log(typography);
 const style = {
-  color: data['body-color'].value,
   fontSize: typography.fontSize.value,
   lineHeight: typography.lineHeight.value,
   letterSpacing: typography.letterSpacing.value,
