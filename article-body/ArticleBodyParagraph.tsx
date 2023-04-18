a10f09e6a998e05e4f6ad7461651ef07cb5b4f8c
Hirofumi Tanigami <86785+undefined@users.noreply.github.com>
2023-04-18 04:26:17 +0000

    Push from f

--- article-body/ArticleBodyParagraph.tsx
+++ article-body/ArticleBodyParagraph.tsx
@@ -1,18 +1,16 @@
-import { css } from '@linaria/core';
 import React from 'react';
 import '../tokens/variables.css';
 import './style.css';
+import data from '../sd-input/src/out.json';
 
-const style = css`
-  text-transform: uppercase;
-  font-family: ${fonts.heading};
-  font-size: ${modularScale(2)};
+const typography = data['body-typography'].value;
 
-  ${hiDPI(1.5)} {
-    font-size: ${modularScale(2.5)};
-  }
-`;
+const style = {
+  fontSize: typography.fontSize,
+  lineHeight: typography.lineHeight,
+  marginBottom: typography.paragraphSpacing,
+};
 
 export default ({ children }) => {
-  return <p className="article-body-paragraph">{children}</p>;
+  return <p style={style}>{children}</p>;
 };
